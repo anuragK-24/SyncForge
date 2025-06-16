fc1f9615f56b36f529bb014e6e198a3248b581f4
anuragK-24 <24sept.anurag@gmail.com>
2025-06-16 23:08:57 +0530

    profile router routes, edit password, edit profile

--- backend/src/routes/profile.js
+++ backend/src/routes/profile.js
@@ -1,31 +1,58 @@
 const express = require("express");
 const profileRouter = express.Router();
-const User = require("../models/user");
 const { userAuth } = require("../middleware/auth");
+const { validateEditProfileData } = require("../utils/validation");
+const bcrypt = require("bcrypt");
 
-profileRouter.get("/fetch", userAuth, async (req, res) => {
+profileRouter.get("/view", userAuth, async (req, res) => {
   try {
-    const users = await User.find();
-    res.send(users).status(200);
-  } catch (error) {
-    console.log("ERROR :", error.message);
-  }
-});
-profileRouter.patch("/edit", userAuth, async (req, res) => {
-  try {
-    const user1 = await User.getJWT();
-    console.log(user1.cookies);
-    const { _id, firstName, lastName, age, skills, photoURL, about } = req.body;
-
-    const patchData = { firstName, lastName, age, skills, photoURL, about };
-    const user = await User.findByIdAndUpdate(_id, patchData, {
-      new: true,
-      runValidators: true,
-    });
+    const user = await req.user;
     res.send(user).status(200);
   } catch (error) {
     console.log("ERROR :", error.message);
   }
 });
 
+profileRouter.patch("/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req.body)) {
+      throw new Error("Invalid Edit Request");
+    }
+    const loggedInUser = req.user;
+    // Update only provided fields
+    Object.keys(req.body).forEach((key) => {
+      loggedInUser[key] = req.body[key];
+    });
+
+    await loggedInUser.save();
+
+    console.log("Updated user:", loggedInUser);
+    res.status(200).send("Profile updated successfully");
+  } catch (err) {
+    console.error("ERROR:", err.message);
+    res.status(400).send("Error updating profile: " + err.message);
+  }
+});
+profileRouter.patch("/editPassword", userAuth, async (req, res) => {
+  try {
+    const { password } = req.body;
+
+    if (!password || password.length < 8) {
+      throw new Error("Invalid password. Must be at least 8 characters.");
+    }
+
+    const loggedInUser = req.user;
+
+    // Hash the new password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    loggedInUser.password = hashedPassword;
+
+    await loggedInUser.save();
+    
+    res.status(200).send(loggedInUser.firstName + ", your Password updated successfully");
+  } catch (err) {
+    console.error("ERROR:", err.message);
+    res.status(500).send("Error updating password: " + err.message);
+  }
+});
 module.exports = profileRouter;
--- backend/src/utils/validation.js
+++ backend/src/utils/validation.js
@@ -2,15 +2,31 @@ const validator = require("validator");
 
 const validateSignUpData = (data) => {
   const { firstName, lastName, emailId, password } = data;
-  
+
   if (!firstName || !lastName) {
-      throw new Error("Name is not valid");
-    } else if (!validator.isEmail(emailId)) {
-        throw new Error("Email is not valid");
-    } else if (!validator.isStrongPassword(password)) {
-        throw new Error("Please enter a strong password");
-    }
-    
+    throw new Error("Name is not valid");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("Email is not valid");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Please enter a strong password");
+  }
 };
 
-module.exports = { validateSignUpData };
+const validateEditProfileData = (data) => {
+  const allowedEditFields = [
+    "age",
+    "firstName",
+    "lastName",
+    "gender",
+    "photoURL",
+    "age",
+    "skills",
+  ];
+  const isEditAllowed = Object.keys(data).every((field) =>
+    allowedEditFields.includes(field)
+  );
+  return isEditAllowed;
+};
+
+
+module.exports = { validateSignUpData, validateEditProfileData };
